14e6723a758c094ba32edfb6ed7c9ff47efbc3f9
Monica-JMGR <101527414+Monica-JMGR@users.noreply.github.com>
2022-08-02 19:39:33 +0200

    actualizacion Streamlit

---
+++ streamlit/main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+import Functions as ft
+
+
+st.set_page_config(page_title='EmancipaTIC', layout='wide', page_icon="./IMG/descarga.jpg")
+
+
+
+#Sidebar option 
+with st.sidebar:
+    selected = option_menu("EmancipaTIC", ['Home', 'Reporting','Prediction'], 
+        icons=['house'], menu_icon="cast", default_index=0)
+    selected
+
+if selected == 'Home':
+    ft.home()
+elif selected == 'Reporting':
+    ft.reporting()
+elif selected == 'Prediction':
+    ft.prediction()
