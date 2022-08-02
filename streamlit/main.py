a3137e48a4012485bc4757e5a03d67e4ac9b3879
Monica-JMGR <101527414+Monica-JMGR@users.noreply.github.com>
2022-08-02 20:26:24 +0200

    update

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit_option_menu as som
+from streamlit_option_menu import option_menu
 import Functions as ft
 
 
@@ -9,7 +9,7 @@ st.set_page_config(page_title='EmancipaTIC', layout='wide', page_icon="./IMG/des
 
 #Sidebar option 
 with st.sidebar:
-    selected = som.option_menu("EmancipaTIC", ['Home', 'Reporting','Prediction'], 
+    selected = option_menu("EmancipaTIC", ['Home', 'Reporting','Prediction'], 
         icons=['house'], menu_icon="cast", default_index=0)
     selected
 
